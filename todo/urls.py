c356759d080aed00c9174760d843c979a7046eaa
Arsen <arsen.test.mess@gmail.com>
2024-11-01 12:14:08 +0200

    add paths create, update and delete

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 
-from todo.views import TaskListView
+from todo.views import TaskListView, TagListView, TaskCreateView, \
+    TaskUpdateView, TaskDeleteView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
+    path("task/create/", TaskCreateView.as_view(), name="task-create"),
+    path("task/<int:pk>/update/",
+         TaskUpdateView.as_view(),
+         name="task-update"),
+    path("task/<int:pk>/delete/",
+         TaskDeleteView.as_view(),
+         name="task-delete"),
+    path("tags/", TagListView.as_view(), name="tags-list"),
 ]
 
 app_name = "todo"
