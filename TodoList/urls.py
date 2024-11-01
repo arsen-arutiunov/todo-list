1973a27e6aaff8718c1ebe5c93501d6e7515fe17
Arsen <arsen.test.mess@gmail.com>
2024-11-01 15:05:56 +0200

    add paths for login and logout

--- TodoList/urls.py
+++ TodoList/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import redirect
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("todo.urls", namespace="todo")),
+    path("accounts/login/", LoginView.as_view(), name="login"),
+    path("accounts/logout/",
+         LogoutView.as_view(),
+         name="logout"),
 ]
