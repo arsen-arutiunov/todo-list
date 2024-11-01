d8b6ff16f9c615c7af3d9921129ce0c85603370c
Arsen <arsen.test.mess@gmail.com>
2024-11-01 12:13:02 +0200

    create class views create, update and delete for Task model

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from django.views import generic
 
 from todo.models import Task, Tag
@@ -7,6 +8,22 @@ from todo.models import Task, Tag
 class TaskListView(LoginRequiredMixin, generic.ListView):
     model = Task
     context_object_name = "task_list"
+    template_name = "todo/index.html"
+
+
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Task
+    success_url = reverse_lazy("todo:index")
+
+
+class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Task
+    success_url = reverse_lazy("todo:index")
+
+
+class TaskDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:index")
 
 
 class TagListView(LoginRequiredMixin, generic.ListView):
