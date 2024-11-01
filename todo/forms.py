61fa56c98acc5e9db23306db16a243e3ad8de4af
Arsen <arsen.test.mess@gmail.com>
2024-11-01 13:36:58 +0200

    add tag form to creating and updating tags

--- todo/forms.py
+++ todo/forms.py
@@ -1,7 +1,14 @@
 from django import forms
-from .models import Task
+from .models import Task, Tag
+
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ["content", "deadline", "done", "tags"]
+
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = ["name"]
