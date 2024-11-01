34ea66b39e01274bcf9ea642aacee5bfd2a05831
Arsen <arsen.test.mess@gmail.com>
2024-11-01 11:48:31 +0200

    create Tag list class view in views

--- todo/views.py
+++ todo/views.py
@@ -2,9 +2,14 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import generic
 
-from todo.models import Task
+from todo.models import Task, Tag
 
 
 class TaskListView(LoginRequiredMixin, generic.ListView):
     model = Task
     context_object_name = "task_list"
+
+
+class TagListView(LoginRequiredMixin, generic.ListView):
+    model = Tag
+    context_object_name = "tag_list"
