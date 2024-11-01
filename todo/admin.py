33bb06edbb3e998fce7aedd91d0ce51ffbc5bb7e
Arsen <arsen.test.mess@gmail.com>
2024-11-01 11:09:29 +0200

    add fields Tag and Task in admin panel

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from todo.models import Tag, Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ("content",)
+    list_filter = ("done", "created_at")
+
+admin.site.register(Tag)
