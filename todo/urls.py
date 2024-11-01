8d1ff581c57293342bd9fa8253135ff2fc53301b
Arsen <arsen.test.mess@gmail.com>
2024-11-01 12:23:30 +0200

    add complete and undo paths

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from todo.views import TaskListView, TagListView, TaskCreateView, \
-    TaskUpdateView, TaskDeleteView
+    TaskUpdateView, TaskDeleteView, complete_task, undo_task
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
@@ -12,6 +12,12 @@ urlpatterns = [
     path("task/<int:pk>/delete/",
          TaskDeleteView.as_view(),
          name="task-delete"),
+    path("task/<int:task_id>/complete/",
+         complete_task,
+         name="task-complete"),
+    path("task/<int:task_id>/undo/",
+         undo_task,
+         name="task-undo"),
     path("tags/", TagListView.as_view(), name="tags-list"),
 ]
 
