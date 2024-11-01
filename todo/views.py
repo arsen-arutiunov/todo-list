ff07c38fb5d5081fdc113f5241aa43b38138b1d1
Arsen <arsen.test.mess@gmail.com>
2024-11-01 11:29:40 +0200

    create TaskList view

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from todo.models import Task
+
+
+class TaskListView(LoginRequiredMixin, generic.ListView):
+    model = Task
+    context_object_name = "task_list"
